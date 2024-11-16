29d0c458b176a1752b205272f1401b0b9c7371ef
0xhatsume <0xhatsume@gmail.com>
2024-11-16 18:46:51 +0700

    :lipstick: Resize navbar for tg

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -13,25 +13,29 @@ export interface NavbarProps {
 export function Navbar ({ isMobileOrTelegram }: NavbarProps) {
     return (
         <div className="border-b">
-            <div className={`${isMobileOrTelegram ? 'max-w-lg' : 'max-w-7xl'} mx-auto px-4 h-16 flex items-center justify-between`}>
-            {/* Logo and Name */}
-            <div className="flex items-center space-x-3">
-                <span className="flex items-center py-8">
-                    <LiaRobotSolid className="h-10 w-10 aspect-square text-primary" />
-                    <GiSharkJaws className="h-10 w-10 aspect-square text-primary" />
-                </span>
-                <span className="text-primary font-bold text-xl">
-                    Crypto.AI.SharkTank
-                    </span>
-            </div>
-    
-            {/* Wallet Connect */}
-            <Button variant="outline" className="flex items-center space-x-2">
-                <Wallet className="h-4 w-4 text-primary" />
-                <span className="text-primary">
-                Connect Wallet
-                </span>
-            </Button>
+            <div className={`${isMobileOrTelegram ? 'max-w-lg' : 'max-w-7xl'} mx-auto px-4 py-3`}>
+                <div className="flex flex-col sm:flex-row sm:items-center sm:h-16 gap-3 sm:gap-0">
+                    {/* Logo and Name */}
+                    <div className="ml-2 flex items-center space-x-3">
+                        <span className="flex items-center py-2">
+                            <LiaRobotSolid className="h-10 w-10 aspect-square text-primary" />
+                            <GiSharkJaws className="h-10 w-10 aspect-square text-primary" />
+                        </span>
+                        <span className="text-primary font-bold text-xl">
+                            Crypto.AI.SharkTank
+                            </span>
+                    </div>
+            
+                    {/* Wallet Connect */}
+                    <Button 
+                        variant="outline" 
+                        className="w-full sm:w-auto sm:ml-auto flex items-center justify-center space-x-2">
+                            <Wallet className="h-4 w-4 text-primary" />
+                            <span className="text-primary">
+                                Connect Wallet
+                                </span>
+                    </Button>
+                </div>
             </div>
         </div>
         );
